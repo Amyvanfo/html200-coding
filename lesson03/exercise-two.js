b2e45751fc32f22d52f85432f8ebb24e45ddccb4
Amyvanfo <25785423+Amyvanfo@users.noreply.github.com>
2019-11-16 14:56:42 -0800

    created exercise 2

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,6 @@
+let treeFact = 'Trees support the environment by providing habitat for birds and small mammals.';
+
+const treeFactArr = treeFact.split(' ');
+
+console.log(treeFact);
+console.log(treeFactArr[2]);
