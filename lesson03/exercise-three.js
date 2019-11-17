f3c5de91dc6963c45d4ba0a826b0d706b3c7015d
Amyvanfo <25785423+Amyvanfo@users.noreply.github.com>
2019-11-16 16:53:02 -0800

    completed exercise three

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,10 @@
+//Create an array that has at least 5 strings:
+
+//Use a for loop to console log all of the elements plus the index and a comma
+
+let arr = ['Ariana', 'Amber', 'Zack', 'Jeff', 'Taylor', 'Stephanie', 'Annie'];
+
+for (let i = 0; i < arr.length; i++)
+{
+console.log(i + ',' + arr[i]);
+}
