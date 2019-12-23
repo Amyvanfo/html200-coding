1f1f9d0a210f62895ef5051f97bbdb5ff568ba9c
Amyvanfo <25785423+Amyvanfo@users.noreply.github.com>
2019-12-22 16:52:08 -0800

    added JS actions for deposit amt and withdraw.

--- bank.js
+++ bank.js
@@ -1,1 +1,19 @@
-let chooseAction = prompt();
+function bankAction() {
+  document.getElementById("deposit").innerHTML = "Enter a deposit amount";
+  let input = prompt('add deposit amount')
+  let result = 0;
+  result = Number(input);
+  alert(result + 1000)
+
+}function withdraw() {
+  document.getElementById("withdraw").innerHTML = "Enter an amount to withdraw";
+  /*let input = prompt('enter withdrawal amount')
+  let result = 0;
+  result = Number(input);
+  alert(1000 - input)*/
+
+}function getBalance() {
+  document.getElementById("balance").innerHTML = "Your Balance is___";
+}function quitApp() {
+  document.getElementById("Quit").innerHTML = "Goodbye";
+}
